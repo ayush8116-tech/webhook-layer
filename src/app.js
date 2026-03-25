9e28c5493569a188494beb94ee47ac39abd41ad9
Ayush Verma <vermajiii8116@gmail.com>
2026-03-25 23:01:26 +0530

    checks the request

--- src/app.js
+++ src/app.js
@@ -8,18 +8,42 @@ export const createApp = () => {
   app.use(logger());
 
   app.post("/github-webhook", async (c) => {
-    const payload = await c.req.body();
+    const payload = await c.req.json();
+    console.log(payload);
+    
     const conclusion = payload["workflow_run"].conclusion;
 
-    await fetch("http://localhost:8000/indicate", {
+    const res = await fetch("http://localhost:8000/indicate", {
       method: "post",
       body: conclusion,
     });
+    console.log(await res.text());
 
     return c.text("light request sent...");
   });
 
+  
   app.get("*", serveStatic({ root: "./public" }));
-
+  
   return app;
 };
+
+
+
+// app.get("/", async (c) => {
+//   const payload = {
+//     "workflow_run": {
+//       conclusion: "success",
+//     },
+//   };
+  
+//   const res = await fetch("http://localhost:2000/github-webhook", {
+//     method: "post",
+//     body : JSON.stringify(payload),
+//     "content-type": "application/json",
+//   });
+
+//   console.log(await res.text());
+
+//   return c.text("request is going to own server");
+// });
