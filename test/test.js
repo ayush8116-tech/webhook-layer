b86494b16b4311a197ff44ee2ec9f9e7534c9a6d
Ayush Verma <vermajiii8116@gmail.com>
2026-03-26 08:12:05 +0530

    adds test

---
+++ test/test.js
@@ -0,0 +1,8 @@
+import { assertEquals } from "@std/assert";
+import { describe, it } from "@std/testing/bdd";
+
+describe("simple test suite", () => {
+  it("simple test", () => {
+    assertEquals(0, 0);
+  })
+})
